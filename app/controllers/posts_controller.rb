3b7029114cad21c29d92d3adcd9de5be5aa068c4
lieby weintraub <liebahinda@gmail.com>
2019-07-10 15:33:20 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,21 @@
 class PostsController < ApplicationController
- def show
+  def show
     @post = Post.find(params[:id])
   end
 
-  
+  def edit
+    @post = Post.find(params[:id])
+  end
 
   def update
     @post = Post.find(params[:id])
-   
-   if  @post.update(post_params)
-redirect_to post_path(@post)
-else 
-  render :edit 
+
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
-end 
 
   private
 
